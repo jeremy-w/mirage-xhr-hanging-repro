a7332c179d7d96374baa269d3d59522e7bc901e2
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-07-13 15:32:37 -0400

    Replace Mirage for Pretender
    Pulled in via ember-cli-pretender because sorting out how to import it
    was a headache.
    
    **The issue still reproduces, so we can rule out Mirage.**

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,16 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+// Disabling appears to prevent export of the `pretender` module, so tests just don't run.
+const PRETENDER_ENABLED = true;
+
 /* global require, module */
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
         emberCLIDeploy: {
           shouldActivate: false
+        },
+        pretender: {
+          enabled: PRETENDER_ENABLED,
         },
     });
 
--- tests/acceptance/mirage-should-allow-xhr-to-complete-test.js
+++ tests/acceptance/mirage-should-allow-xhr-to-complete-test.js
@@ -1,14 +1,23 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'mirage-freezes/tests/helpers/module-for-acceptance';
 import Ember from 'ember';
+import Pretender from 'pretender';
 
 moduleForAcceptance('Acceptance | Mirage should allow XHR to complete', {
   beforeEach() {
+    this.pretender = new Pretender();
+    var counter = 0;
+    this.pretender.get('/', function() {
+      var value = ++counter;
+      console.log('mirage: intercepted XHR GET for "/": returning response forthwith');
+      return 'Mirage Response #' + value + ' Appears';
+    });
     console.log('lo, a new test begins');
   },
 
   afterEach() {
     console.log('and so the test finishes');
+    this.pretender.shutdown();
   }
 });
 
