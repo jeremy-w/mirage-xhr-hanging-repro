136169489345f272e55ccef5c144da5cb7dc0bd5
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-07-13 14:32:44 -0400

    Remove ember-intl service injection

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  intl: Ember.inject.service(),
-  session: Ember.inject.service(),
-  layout: Ember.inject.service('device/layout'),
 });
